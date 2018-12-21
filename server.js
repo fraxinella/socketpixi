663a02da52630a431081f64e3cca2130d81eb4f6
Christopher A. Butler <zionviller@gmail.com>
2018-12-20 22:51:12 -0800

    First Commit.

---
+++ server.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const zoom = 8;
+
+stage = {
+    width: 1024,
+    height: 512
+}
+
+const allUsers = {};
+const inputPack = {};
+const ball = {
+    x: 512,
+    y: 256,
+    width: 8,
+    height: 8
+}
+
+app.use(express.static('static'));
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('User: ', socket.id, ' connected.');
+    socket.broadcast.emit('playerConnect', socket.id);
+    allUsers[socket.id] = {
+        x: 0,
+        y: 0
+    }
+    
+    inputPack[socket.id] = {
+        x:0,
+        y:0
+    }
+    
+    socket.emit('initPositions', allUsers);
+
+    socket.on('playerInput', (data) => {
+        inputPack[data.id] = {x: data.x, y: data.y};
+    })
+    
+    socket.on('disconnect', () => {
+        console.log('User: ', socket.id, ' disconnected.');
+        // console.log("all users: ", allUsers);
+        socket.broadcast.emit('playerDisconnect', socket.id);
+        delete allUsers[socket.id];
+        delete inputPack[socket.id];
+    });
+});
+
+setInterval(() => {
+    for(let id in allUsers){
+        // console.log("INPUT:",inputPack[id]);
+        allUsers[id].x += (inputPack[id].x - allUsers[id].x) * .25;
+        allUsers[id].y += (inputPack[id].y - allUsers[id].y) * .25;
+    }
+}, 1000 / 10)
+
+setInterval(()=>{
+    // console.log("PACK: ", positionPack);
+    io.emit('updatePositions', allUsers);
+}, 1000 / 10)
+
+http.listen(7777, () => {
+    console.log('Listening on 7777');
+})
---
+++ static/js/main.js
@@ -0,0 +1,146 @@
+console.log("\n*** MAIN.JS ***")
+var id = null;
+var destination = null;
+const sprites = {}
+var positions = {}
+const zoom = 4;
+
+const app = new PIXI.Application({
+    width: 1024, 
+    height: 512,                       
+    antialias: false, 
+    transparent: false,
+    resolution: 1,
+    //view: document.getElementById("game")
+})
+
+document.body.appendChild(app.view);
+
+app.renderer.backgroundColor = 0xe0afb8;
+app.renderer.view.style.position = "absolute";
+app.renderer.view.style.display = "block";
+app.renderer.view.style.margin = "auto";
+app.renderer.autoResize = true;
+app.renderer.resize(window.innerWidth, window.innerHeight);
+
+window.addEventListener('resize', ()=> {
+    app.renderer.resize(window.innerWidth, window.innerHeight);
+
+})
+
+PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
+
+console.log("\n*** LOADING GRAPHICS ***");
+PIXI.loader
+.add("/img/persephone.png")
+.add("/img/cursor.png")
+.add("/img/coin.png")
+.load(start);
+
+function start() {
+    console.log("\n*** Startup! ***")
+    console.log("All Positions", positions);
+    
+    app.stage.addChild(new PIXI.Text('DojoNorth: 152.117.208.10:7777', {fontSize: "8em", fill: "#BE6B7A"}));
+    destination = new PIXI.Sprite(PIXI.loader.resources["/img/cursor.png"].texture);
+    destination.scale.set(2,2);
+    app.stage.addChild(destination);
+    
+    for(let _id in positions){
+        if(!sprites[_id]){
+            console.log('adding sprite', _id);
+            sprites[_id] = new PIXI.Sprite(PIXI.loader.resources["/img/persephone.png"].texture);    
+            app.stage.addChild(sprites[_id]);
+            sprites[_id].scale.set(zoom, zoom);
+            sprites[_id].position.set(positions.x, positions.y)
+            app.stage.addChild(sprites[_id]);
+        }
+    }
+    
+    positions[id] = {x: destination.x, y: destination.y };
+
+    console.log("DA SPRITES", sprites);
+    console.log("\n*** Start Game Loop: ***");
+    
+    // window.addEventListener("resize", (event) => {
+    //     app.renderer.resize(window.innerWidth, window.innerHeight);
+    // });
+
+    socket.emit('playerInput', {id: id, x: destination.x, y: destination.y})
+    gameLoop();
+}
+
+const gameLoop = () => {
+    for(let _id in sprites){
+        if(positions[_id]) sprites[_id]['x'] += (positions[_id]['x'] - sprites[_id]['x']) * .1;
+        if(positions[_id]) sprites[_id]['y'] += (positions[_id]['y'] - sprites[_id]['y']) * .1;
+    }
+    requestAnimationFrame(gameLoop);
+}
+
+const socket = io();
+
+socket.on('connect', () => {
+    console.log("\n*** CONNECTED TO SOCKET *** ", socket.id);
+    id = socket.id,
+    console.log(id);
+    
+    
+    
+    document.addEventListener('mousemove', ()=>{
+        if(destination){
+            destination.x = app.renderer.plugins.interaction.mouse.global.x;
+            destination.y = app.renderer.plugins.interaction.mouse.global.y;
+        }
+        // console.log('mouse moving!', positions[id]);
+    })
+    
+    
+});
+
+socket.on('initPositions', (data) => {
+    console.log('\n*** Initializing Positions ***');
+    positions = data;
+    console.log(positions)
+})
+
+socket.on('updatePositions', (newPositions)=> {
+    // console.log("updating positions", newPositions);
+    positions = newPositions;
+    // console.log('\n** POS UPDATE **', newPositions);
+    for(let _id in positions) {
+        //create a sprite if new entity
+        // if(!sprites[_id]){
+        //     // console.log('adding sprite', _id);
+        //     sprites[_id] = new PIXI.Sprite(PIXI.loader.resources["/img/persephone.png"].texture);    
+        //     app.stage.addChild(sprites[_id]);
+        //     sprites[_id].scale.set(8,8);
+        //     sprites[_id].position.set(positions.x, positions.y)
+        //     // console.log("new sprites position", sprites[_id].position);
+        // }
+        // console.log('ID: ', _id, ' Sprites: ', sprites);
+    }
+    // console.log("All Positions: ",positions)
+    if(destination) socket.emit('playerInput', {id: id, x: destination.x, y: destination.y})
+})
+
+socket.on('playerConnect', (playerId) => {
+    console.log('Player Connected: ', playerId);
+    console.log('adding sprite', playerId);
+            sprites[playerId] = new PIXI.Sprite(PIXI.loader.resources["/img/persephone.png"].texture);    
+            app.stage.addChild(sprites[playerId]);
+            sprites[playerId].scale.set(zoom, zoom);
+            sprites[playerId].position.set(positions.x, positions.y)
+            // console.log("new sprites position", sprites[playerId].position);
+            app.stage.addChild(sprites[playerId]);
+})
+
+socket.on('playerDisconnect', (playerId) => {
+    console.log('Player Disconected: ', playerId);
+    app.stage.removeChild(sprites[playerId]);
+    delete sprites[playerId];
+    delete positions[playerId];
+})
+
+
+    
